58ba0c3cadf6410a4281322b5a31df5bea475234
Aleksandar <aca801@gmail.com>
2019-11-10 15:57:59 +0100

    Initial commit with migration added

---
+++ SimpleImageGallery/Models/GalleryIndexModel.cs
@@ -0,0 +1,15 @@
+﻿using SimpleImageGallery.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleImageGallery.Models
+{
+    public class GalleryIndexModel
+    {
+        public IEnumerable<GalleryImage> Images { get; set; }
+        public string SearchQuey { get; set; }
+
+    }
+}
